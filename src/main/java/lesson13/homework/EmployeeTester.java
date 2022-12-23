478e4d9f49cb7c87d1c894f31360f57554b486f5
Ivan Zamula <ivan.zamula@gmail.com>
2022-12-23 21:49:42 +0300

    + Lesson 13 + homework

---
+++ src/main/java/lesson13/homework/EmployeeTester.java
@@ -0,0 +1,24 @@
+package lesson13.homework;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
+public class EmployeeTester {
+    public static void main(String[] args) {
+        List<Employee> employees = new ArrayList<>(
+                Arrays.asList(
+                        new Employee(1,"Ivan Ivanov", 25, 25000),
+                        new Employee(2, "Sergey Ivanov", 33, 28000),
+                        new Employee(2, "Oleg Ivanov", 33, 30000),
+                        new Employee(3, "Olga Semenova", 24, 13500),
+                        new Employee(4, "Igor Orlov", 56, 48000)
+                )
+        );
+
+        System.out.println(employees);
+        employees.sort(Comparator.comparing(Employee::getAge).thenComparing(Employee::getName));
+        System.out.println(employees);
+    }
+}
